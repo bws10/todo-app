350327324ca0e94618a1b99f132c3280c025cc80
bws10 <58655908+bws10@users.noreply.github.com>
2021-10-05 00:28:35 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -2,12 +2,53 @@ import React, { useState, useRef, useEffect } from "react";
 import "./css/App.css";
 import TodoList from "./TodoList";
 import { v4 as uuidv4 } from "uuid";
+import FilterButton from "./FilterButton";
 
 const LOCAL_STORAGE_KEY = "todoApp.todos";
+const FILTER_MAP = {
+  All: () => true,
+  Active: (todo) => !todo.complete,
+  Completed: (todo) => todo.complete,
+};
+const FILTER_NAMES = Object.keys(FILTER_MAP);
 
 export default function App() {
   const [todos, setTodos] = useState([]);
   const todoNameRef = useRef();
+  const [filter, setFilter] = useState("All");
+  const filterList = FILTER_NAMES.map((name) => (
+    <FilterButton
+      key={name}
+      name={name}
+      isPressed={name === filter}
+      setFilter={setFilter}
+    />
+  ));
+
+  const useMatchMedia = (mediaQuery, initialValue) => {
+    const [isMatching, setIsMatching] = useState(initialValue);
+    useEffect(() => {
+      const watcher = window.matchMedia(mediaQuery);
+      setIsMatching(watcher.matches);
+      const listener = (matches) => {
+        setIsMatching(matches.matches);
+      };
+      if (watcher.addEventListener) {
+        watcher.addEventListener("change", listener);
+      } else {
+        watcher.addListener(listener);
+      }
+      return () => {
+        if (watcher.removeEventListener) {
+          return watcher.removeEventListener("change", listener);
+        } else {
+          return watcher.removeListener(listener);
+        }
+      };
+    }, [mediaQuery]);
+
+    return isMatching;
+  };
 
   useEffect(() => {
     const storedItems = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
@@ -39,20 +80,38 @@ export default function App() {
     todoNameRef.current.value = null;
   }
 
+  const isMobileResolution = useMatchMedia("(max-width:665px)", true);
+
   return (
     <main>
       <div className="App">
         <h1 className="header">TODO</h1>
         <form className="new__todo" onSubmit={handleNewTodo}>
-          <input
-            ref={todoNameRef}
-            type="text"
-            placeholder="Create a new todo..."
-          />
+          <label htmlFor="newTodo" className="input__div">
+            <span id="newCheck" className="custom__checkbox"></span>
+            <input
+              id="newTodo"
+              ref={todoNameRef}
+              type="text"
+              placeholder="Create a new todo..."
+            />
+          </label>
         </form>
         <div className="list__container">
-          <TodoList todos={todos} setTodos={setTodos} toggleTodo={toggleTodo} />
+          <TodoList
+            FILTER_MAP={FILTER_MAP}
+            useMatchMedia={useMatchMedia}
+            filter={filter}
+            setFilter={setFilter}
+            filterList={filterList}
+            todos={todos}
+            setTodos={setTodos}
+            toggleTodo={toggleTodo}
+          />
         </div>
+        {isMobileResolution && (
+          <div className="controls filters">{filterList}</div>
+        )}
       </div>
     </main>
   );
--- src/TodoList.js
+++ src/TodoList.js
@@ -1,24 +1,17 @@
-import React, { useState } from "react";
+import React from "react";
 import Todo from "./Todo";
-import FilterButton from "./FilterButton";
 
-const FILTER_MAP = {
-  All: () => true,
-  Active: (todo) => !todo.complete,
-  Completed: (todo) => todo.complete,
-};
-const FILTER_NAMES = Object.keys(FILTER_MAP);
-
-export default function TodoList({ todos, toggleTodo, setTodos }) {
-  const [filter, setFilter] = useState("All");
-  const filterList = FILTER_NAMES.map((name) => (
-    <FilterButton
-      key={name}
-      name={name}
-      isPressed={name === filter}
-      setFilter={setFilter}
-    />
-  ));
+export default function TodoList({
+  todos,
+  toggleTodo,
+  setTodos,
+  filter,
+  setFilter,
+  filterList,
+  useMatchMedia,
+  FILTER_MAP,
+}) {
+  const isDesktopResolution = useMatchMedia("(min-width:665px)", true);
 
   function handleClearCompleted() {
     const newTodos = todos.filter((todo) => !todo.complete);
@@ -43,8 +36,8 @@ export default function TodoList({ todos, toggleTodo, setTodos }) {
         <div className="items__left">
           {todos.filter((todo) => !todo.complete).length} items left
         </div>
-        {filterList}
-        <button className="btn btn__clear" onClick={handleClearCompleted}>
+        {isDesktopResolution && filterList}
+        <button className="btn btn--clear" onClick={handleClearCompleted}>
           Clear Completed
         </button>
       </div>
