895103ce6dcba04d05d942d4e5d8cd631bdc94c9
bws10 <58655908+bws10@users.noreply.github.com>
2021-10-12 00:55:52 +0100

    added drag and drop functionality

--- src/Todo.js
+++ src/Todo.js
@@ -1,20 +1,34 @@
 import React from "react";
 
-export default function Todo({ todo, toggleTodo }) {
+export default function Todo(props) {
   function handleTodoCheck() {
-    toggleTodo(todo.id);
+    props.toggleTodo(props.todo.id);
   }
   return (
-    <div onClick={handleTodoCheck} className="todo__item">
+    <div
+      onClick={handleTodoCheck}
+      draggable
+      key={props.todo.id}
+      onDragStart={(e) => props.handletDragStart(e, props.todoI)}
+      onDragEnter={
+        props.dragging
+          ? (e) => {
+              props.handleDragEnter(e, props.todoI);
+            }
+          : null
+      }
+      className={props.dragging ? props.getStyles(props.todoI) : "todo__item"}
+    >
+      {/* {console.log(props.todoI)} */}
       <input
         type="checkbox"
-        id={todo.id}
-        checked={todo.complete}
+        id={props.todo.id}
+        checked={props.todo.complete}
         onChange={handleTodoCheck}
       />
       <label>
-        <span htmlFor={todo.id} className="custom__checkbox"></span>
-        {todo.name}
+        <span htmlFor={props.todo.id} className="custom__checkbox"></span>
+        {props.todo.name}
       </label>
     </div>
   );
--- src/TodoList.js
+++ src/TodoList.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useRef, useEffect } from "react";
 import Todo from "./Todo";
 
 export default function TodoList({
@@ -19,28 +19,87 @@ export default function TodoList({
     setFilter("All");
   }
 
-  return (
-    <>
-      {todos.filter(FILTER_MAP[filter]).map((todo) => {
-        return (
-          <Todo
-            key={todo.id}
-            todo={todo}
-            toggleTodo={toggleTodo}
-            setTodos={setTodos}
-          />
-        );
-      })}
+  const [list, setList] = useState(todos);
+  const [dragging, setDragging] = useState(false);
 
-      <div className="controls">
-        <div className="items__left">
-          {todos.filter((todo) => !todo.complete).length} items left
+  useEffect(() => {
+    setList(todos);
+  }, [setList, todos]);
+
+  const dragItem = useRef();
+  const dragItemNode = useRef();
+
+  const handletDragStart = (e, item) => {
+    console.log("Starting to drag", item);
+
+    dragItemNode.current = e.target;
+    dragItemNode.current.addEventListener("dragend", handleDragEnd);
+    dragItem.current = item;
+
+    setTimeout(() => {
+      setDragging(true);
+    }, 0);
+  };
+  const handleDragEnter = (e, targetItem) => {
+    console.log("Entering a drag target", targetItem);
+    if (dragItemNode.current !== e.target) {
+      console.log("Target is NOT the same as dragged item");
+      setTodos((oldList) => {
+        let newList = JSON.parse(JSON.stringify(oldList));
+
+        newList.splice(targetItem, 0, newList.splice(dragItem.current, 1)[0]);
+        dragItem.current = targetItem;
+        console.log(newList);
+        // localStorage.setItem("List", JSON.stringify(newList));
+        return newList;
+      });
+    }
+  };
+  const handleDragEnd = (e) => {
+    setDragging(false);
+    dragItem.current = null;
+    dragItemNode.current.removeEventListener("dragend", handleDragEnd);
+    dragItemNode.current = null;
+  };
+  const getStyles = (item) => {
+    if (dragItem.current === item) {
+      return "todo__item current";
+    }
+    return "todo__item";
+  };
+
+  if (list) {
+    return (
+      <div className="list__container">
+        {todos.filter(FILTER_MAP[filter]).map((todo, todoI) => {
+          return (
+            <Todo
+              key={todo.id}
+              todo={todo}
+              todoI={todoI}
+              toggleTodo={toggleTodo}
+              setTodos={setTodos}
+              dragging={dragging}
+              handletDragStart={handletDragStart}
+              getStyles={getStyles}
+              handleDragEnd={handleDragEnd}
+              handleDragEnter={handleDragEnter}
+            />
+          );
+        })}
+
+        <div className="controls">
+          <div className="items__left">
+            {todos.filter((todo) => !todo.complete).length} items left
+          </div>
+          {isDesktopResolution && filterList}
+          <button className="btn btn--clear" onClick={handleClearCompleted}>
+            Clear Completed
+          </button>
         </div>
-        {isDesktopResolution && filterList}
-        <button className="btn btn--clear" onClick={handleClearCompleted}>
-          Clear Completed
-        </button>
       </div>
-    </>
-  );
+    );
+  } else {
+    return null;
+  }
 }
